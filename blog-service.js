d13545a79a102971a9fea547f1d0126343e296a9
Manoj <khatrimanoj2934@gmail.com>
2023-06-19 20:19:17 -0400

    initial commit assignment-3

--- blog-service.js
+++ blog-service.js
@@ -1,65 +1,130 @@
 const fs = require('fs');
-const path = require('path');
+const { resolve } = require('path');
+const path = require("path");
 
 let posts = [];
 let categories = [];
 
 function initialize() {
-  return new Promise((resolve, reject) => {
-    const postsFilePath = path.join(__dirname, 'data', 'posts.json');
-    const categoriesFilePath = path.join(__dirname, 'data', 'categories.json');
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, "data", "posts.json"), 'utf8', (err, data) => {
+            if (err) {
+              reject("Unable to read posts file");
+            }
 
-    fs.readFile(postsFilePath, 'utf8', (err, postData) => {
-      if (err) {
-        reject('Unable to read posts file');
-        return;
-      }
+            posts = JSON.parse(data);
 
-      posts = JSON.parse(postData);
+            fs.readFile(path.join(__dirname, "data", "categories.json"), 'utf8', (err, data) => {
+                if (err) {
+                  reject("Unable to read categories file");
+                }
 
-      fs.readFile(categoriesFilePath, 'utf8', (err, categoriesData) => {
-        if (err) {
-          reject('Unable to read categories file');
-          return;
-        }
+                categories = JSON.parse(data);
 
-        categories = JSON.parse(categoriesData);
-        resolve();
-      });
-    });
-  });
+                resolve();
+              });
+          });
+    })
 }
 
 function getAllPosts() {
-  return new Promise((resolve, reject) => {
-    if (posts.length === 0) {
-      reject('No posts available');
-    } else {
-      resolve(posts);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (posts.length === 0) {
+            reject("No results returned");
+        } else {
+            resolve(posts);
+        }
+    })
 }
 
 function getPublishedPosts() {
-  return new Promise((resolve, reject) => {
-    const publishedPosts = posts.filter(post => post.published);
+    return new Promise((resolve, reject) => {
+        let publishedPosts = [];
+        posts.forEach((post) => {
+            if (post.published === true) {
+                publishedPosts.push(post);
+            }
+        })
 
-    if (publishedPosts.length > 0) {
-      resolve(publishedPosts);
-    } else {
-      reject('No published posts available');
-    }
-  });
+        if (publishedPosts.length > 0) {
+            resolve(publishedPosts);
+        } else {
+            reject("No results returned");
+        }
+    })    
 }
 
 function getCategories() {
-  return new Promise((resolve, reject) => {
-    if (categories.length === 0) {
-      reject('No categories available');
-    } else {
-      resolve(categories);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (categories.length === 0) {
+            reject("No results returned");
+        } else {
+            resolve(categories);
+        }
+    })
 }
 
-module.exports = { initialize, getAllPosts, getPublishedPosts, getCategories };
+function getPostById(id) {
+    return new Promise((resolve, reject) => {
+        const filteredPosts = posts.filter(post => post.id == id);
+        const uniquePost = filteredPosts[0];
+
+        if (uniquePost) {
+            resolve(uniquePost);
+        }
+        else {
+            reject("no result returned");
+        }
+    })
+}
+
+function getPostsByCategory(category) {
+    return new Promise((resolve, reject) => {
+        const filteredPosts = posts.filter(post => post.category == category);
+
+        if (filteredPosts.length > 0) {
+            resolve(filteredPosts);
+        } else {
+            reject("no results returned");
+        }
+    })
+}
+
+function getPostsByMinDate(minDate) {
+    return new Promise((resolve, reject) => {
+        const filteredPosts = posts.filter(post => new Date(post.postDate) >= new Date(minDate));
+
+        if (filteredPosts.length > 0) {
+            resolve(filteredPosts);
+        } else {
+            reject("no results returned");
+        }
+    })
+}
+
+function addPost(postData) {
+    return new Promise((resolve, reject) => {
+        if (postData.published === undefined) {
+            postData.published = false;
+        } else {
+            postData.published = true;
+        }
+    
+        postData.id = posts.length + 1;
+    
+        posts.push(postData);
+        resolve(postData);
+    })
+    
+}
+
+module.exports = { 
+    initialize, 
+    getAllPosts, 
+    getPublishedPosts, 
+    getCategories, 
+    addPost, 
+    getPostById,
+    getPostsByCategory,
+    getPostsByMinDate
+};
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
 const path = require("path");
-const { initialize, getAllPosts, getPublishedPosts, getCategories } = require("./blog-service.js");
+const {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories,
+  addPost,
+  getPostById,
+  getPostsByCategory,
+  getPostsByMinDate
+} = require("./blog-service.js");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+cloudinary.config({ 
+  cloud_name: 'dyee1c4hb', 
+  api_key: '413588338766645', 
+  api_secret: 'dyXIekZDmtq-e8gP7qOCr2BceKg' 
+});
+
+const upload = multer();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -27,14 +47,92 @@ app.get("/blog", (req, res) => {
 });
 
 app.get("/posts", (req, res) => {
-  getAllPosts()
+  if (req.query.category) {
+    getPostsByCategory(req.query.category)
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       res.send(err);
     });
+  }
+
+  else if (req.query.minDate) {
+    getPostsByMinDate(req.query.minDate)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  }
+
+  else {
+    getAllPosts()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  }
 });
+
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addPost.html"));
+})
+
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    return result;
+  }
+
+  upload(req)
+  .then((uploaded) => {
+    req.body.featureImage = uploaded.url;
+    let postObject = {};
+
+    postObject.body = req.body.body;
+    postObject.title = req.body.title;
+    postObject.postDate = Date.now();
+    postObject.category = req.body.category;
+    postObject.featureImage = req.body.featureImage;
+    postObject.published = req.body.published;
+    
+    if (postObject.title) {
+      addPost(postObject);
+    }
+    res.redirect("/posts");
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
+
+app.get("/post/:value", (req, res) => {
+  getPostById(req.params.value)
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+})
 
 app.get("/categories", (req, res) => {
   getCategories()
@@ -50,12 +148,8 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "notFoundPage.html"));
 });
 
-initialize()
-  .then(() => {
-    app.listen(HTTP_PORT, () => {
-      console.log("Express server listening on port " + HTTP_PORT);
-    });
-  })
-  .catch((err) => {
-    console.error("Initialization failed: ", err);
+initialize().then(() => {
+  app.listen(HTTP_PORT, () => {
+    console.log("Express http server listening on: http://localhost:" + HTTP_PORT);
   });
+});
